4fc797d5b7869f9d386df2b823168ff76f1359e6
mr <mr@mrautenberg.de>
2011-09-20 13:38:56 +0200

    feature: model registration to ontology service

--- lib/ontology.rb
+++ lib/ontology.rb
@@ -17,7 +17,7 @@ module OpenTox
             ?endpoint dc:title ?title.
           }")
       end
-      
+
       def self.make_option_list(endpoint="Endpoints", level=1)
       out = ""
         results = echa_endpoints(endpoint) rescue results = []
@@ -30,7 +30,7 @@ module OpenTox
         end
         return out
       end
-    
+
       def self.endpoint_option_list(include_blank=true)
         out = "<select id='endpoint' name='endpoint'>\n"
         out += "<option value='' id='please_select'>Please select</option>\n" if include_blank 
@@ -64,25 +64,72 @@ module OpenTox
 
     end
 
-    # Register an OpenTox resource into ontology service
-    # @param [String] uri, URI of recource to register
-    # @param [String] subjectid
-    def self.register(uri, subjectid=nil)
-      begin
-        RestClientWrapper.post(ONTOLOGY_SERVER, {:uri => uri}, {:subjectid => CGI.escape(subjectid)})
-      rescue
-        false
-      end
-    end
+    #Model Class for OpenTox::Ontology to register/deregister and check models in the ontology service
+    #@example Register a model URI to the ontology service, check if model URI exists and delete it 
+    #  uri = "http://mymodelservice.net/model/1" # model uri will be checked by the ontology service itself
+    #  OpenTox::Ontology::Model.register(uri)
+    #  puts OpenTox::Ontology::Model.exists?(uri) # => should return true
+    #  OpenTox::Ontology::Model.delete(uri)
+    #  puts OpenTox::Ontology::Model.exists?(uri) # => should return false
+    module Model
 
-    # Unregister an OpenTox resource into ontology service
-    # @param [String] uri, URI of recource to unregister
-    # @param [String] subjectid
-    def self.unregister(uri, subjectid=nil)
-      begin
-        RestClientWrapper.delete("#{ONTOLOGY_SERVER}?uri=#{CGI.escape(uri)}", {:subjectid => CGI.escape(subjectid)})
-      rescue
-        false
+      # Register an OpenTox resource into ontology service
+      # @param [String] uri, URI of recource to register
+      # @param [String] subjectid
+      def self.register(uri, subjectid=nil)
+        begin
+          RestClientWrapper.post(ONTOLOGY_SERVER, {:uri => uri}, {:subjectid => CGI.escape(subjectid)})
+        rescue
+          LOGGER.warn "OpenTox::Ontology::Model.register ontology service is not reachable. Failed to register URI: #{uri} with subjectid: #{subjectid}"
+          false
+        end
+      end
+
+      # Deregister an OpenTox resource into ontology service
+      # @param [String] uri, URI of recource to deregister/delete
+      # @param [String] subjectid
+      def self.delete(uri, subjectid=nil)
+        begin
+          RestClientWrapper.delete("#{ONTOLOGY_SERVER}?uri=#{CGI.escape(uri)}", {:subjectid => CGI.escape(subjectid)})
+        rescue
+          LOGGER.warn "OpenTox::Ontology::Model.exists ontology service is not reachable. Failed to delete URI: #{uri} with subjectid: #{subjectid}"
+          false
+        end
+      end
+
+      # Find an OpenTox resource in the ontology service
+      # @param [String] uri, URI of recource to find
+      # @param [String] subjectid
+      def self.exists?(uri, subjectid=nil)
+        begin
+          out = RestClientWrapper.get("#{ONTOLOGY_SERVER}?query=#{querystring(uri)}",:accept => "text/csv").collect{|l| l.gsub("\r\n", "") if l.match(/^http/)}.uniq.compact
+          return true if out.size > 0
+          false
+        rescue
+          LOGGER.warn "OpenTox::Ontology::Model.exists ontology service is not reachable. Failed to check for URI: #{uri} with subjectid: #{subjectid}"
+          false
+        end
+      end
+
+      private
+      # Query string to find a registered model
+      # @param [String] uri, model URI
+      def self.querystring(uri)
+        return CGI.escape("PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX owl:<http://www.w3.org/2002/07/owl#>
+        PREFIX dc:<http://purl.org/dc/elements/1.1/>
+        PREFIX dcterms:<http://purl.org/dc/terms/>
+        PREFIX ot:<http://www.opentox.org/api/1.1#>
+        select distinct ?model ?title ?creator ?trainingDataset ?algorithm
+          where {
+          ?model rdf:type ot:Model;
+            OPTIONAL {?model dc:title ?title}.
+            OPTIONAL {?model dc:creator ?creator}.
+            OPTIONAL {?model ot:trainingDataset ?trainingDataset}.
+            OPTIONAL {?model ot:algorithm ?algorithm }.
+            FILTER (?model = <#{uri}>)
+          }")
       end
     end
 
