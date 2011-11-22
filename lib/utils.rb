b0119a7979a4429a146cc2983ef57e44f28ece59
Andreas Maunz <andreas@maunz.de>
2011-11-22 13:41:41 +0100

    Started PC calc

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,6 +1,6 @@
 module OpenTox
   module Algorithm
-   
+
     # Gauss kernel
     # @return [Float] 
     def self.gauss(x, sigma = 0.3) 
@@ -14,8 +14,8 @@ module OpenTox
     def self.isnull_or_singular?(array)
       nr_zeroes = array.count(0)
       return (nr_zeroes == array.size) ||    # remove non-occurring feature
-             (nr_zeroes == array.size-1) ||  # remove singular feature
-             (nr_zeroes == 0)                # also remove feature present everywhere
+        (nr_zeroes == array.size-1) ||  # remove singular feature
+        (nr_zeroes == 0)                # also remove feature present everywhere
     end
 
     # Numeric value test
@@ -31,7 +31,7 @@ module OpenTox
     def self.zero_variance?(array)
       return (array.to_scale.variance_population == 0.0)
     end
-    
+
     # Sum of an array for Arrays.
     # @param [Array] Array with values
     # @return [Integer] Sum of size of values
@@ -64,20 +64,20 @@ module OpenTox
         expected = db_instances[i].to_f/nr_db
         if actual > expected
           if ((actual - expected) / actual) > max_value
-           max_value = (actual - expected) / actual # 'Schleppzeiger'
+            max_value = (actual - expected) / actual # 'Schleppzeiger'
             max = i
           end
         end
       }
       max
     end
-    
+
     # Returns Support value of an fingerprint
     # @param [Hash] params Keys: `:compound_features_hits, :weights, :training_compound_features_hits, :features, :nr_hits:, :mode` are required
     # return [Numeric] Support value 
     def self.p_sum_support(params)
       p_sum = 0.0
-        params[:features].each{|f|
+      params[:features].each{|f|
         compound_hits = params[:compound_features_hits][f]
         neighbor_hits = params[:training_compound_features_hits][f] 
         p_sum += eval("(Algorithm.gauss(params[:weights][f]) * ([compound_hits, neighbor_hits].compact.#{params[:mode]}))")
@@ -89,11 +89,11 @@ module OpenTox
 
       # Calculate the propositionalization matrix (aka instantiation matrix) via fingerprints.
       # Same for the vector describing the query compound.
-      # @param[Hash] Required keys: :neighbors, compound, features, nr_hits, fingerprints, p_values
+      # @param[Hash] Required keys: :neighbors, :compound, :features, nr_hits, :fingerprints, :p_values
       def self.get_props_fingerprints (params)
         matrix = Array.new
         begin 
-          
+
           # neighbors
           params[:neighbors].each do |n|
             n = n[:compound]
@@ -114,7 +114,7 @@ module OpenTox
             end
             matrix << row if row_good
           end
-      
+
           row = []
           params[:features].each do |f|
             if params[:nr_hits]
@@ -129,8 +129,93 @@ module OpenTox
         end
         [ matrix, row ]
       end
-      
-      
+
+      # Calculate the propositionalization matrix (aka instantiation matrix) via physico-chemical descriptors.
+      # Same for the vector describing the query compound.
+      # The third argument takes a string from {"geometrical", "topological", "electronic", "constitutional", "hybrid" } as in ambit_descriptors.yaml
+      # @param[Hash] Required keys: :neighbors, :compound, :pc_group
+      def self.get_props_pc(params)
+
+        ambit_ds_service_uri = "http://apps.ideaconsult.net:8080/ambit2/dataset/"
+        descs = YAML::load_file( File.join(ENV['HOME'], ".opentox", "config", "ambit_descriptors.yaml") )
+        descs_uris = []
+
+        descs.each { |uri, cat_name| 
+          if cat_name[:category] == params[:pc_group]
+            descs_uris << uri
+          end
+        }
+        if descs_uris.size == 0
+          raise "Error! Empty set of descriptors. Did you supply one of [geometrical, topological, electronic, constitutional, hybrid] ?"
+        end
+        LOGGER.debug "Ambit descriptor URIs: #{descs_uris.join(", ")}"
+
+
+        begin
+
+          smiles = []
+          params[:neighbors].each do |n|
+            smiles << OpenTox::Compound.new(n[:compound]).to_smiles
+          end
+          smiles << OpenTox::Compound.new(params[:compound]).to_smiles
+
+          smi_file = Tempfile.open(['pc_ambit', '.smi'])
+          pc_descriptors = nil
+
+          begin
+
+            # Create Ambit dataset
+            smi_file.puts( smiles.join("\n") )
+            smi_file.close
+            ambit_ds_uri = OpenTox::RestClientWrapper.post(ambit_ds_service_uri, {:file => File.new(smi_file.path)}, {:content_type => "multipart/form-data", :accept => "text/uri-list"} )
+
+            # Get Ambit results
+            ambit_result_uri = ambit_ds_uri + "?"
+            descs_uris.each_with_index do |uri, i|
+              algorithm = OpenTox::Algorithm::Generic.new(uri)
+              result_uri = algorithm.run({:dataset_uri => ambit_ds_uri})
+              ambit_result_uri += result_uri.split("?")[1] + "&"
+              LOGGER.debug "Ambit (#{descs_uris.size}): #{i+1}"
+            end
+            pc_descriptors = OpenTox::Dataset.find(ambit_result_uri)
+
+            # Translate compounds back
+            ambit2ist = {}
+            pc_descriptors.compounds.each do |ambit_compound|
+              compound = OpenTox::Compound.new(ambit_compound)
+              ambit2ist[ambit_compound] = OpenTox::Compound.from_inchi(compound.to_inchi).uri
+            end
+            pc_descriptors.compounds.replace(ambit2ist.values)
+            ambit2ist.values.each do |c|
+              pc_descriptors.data_entries[c] = pc_descriptors.data_entries[ambit2ist.index(c)]
+              pc_descriptors.data_entries.delete(ambit2ist.index(c))
+            end
+          ensure
+            smi_file.unlink
+          end
+
+          matrix = []
+          begin 
+            pc_descriptors.compounds.each { |c|
+              row = []
+              pc_descriptors.features.keys.each { |f|
+                entry = pc_descriptors.data_entries[c][f]
+                row << (entry.nil? ? nil : entry[0])
+              } 
+              matrix << row
+            }
+          end
+
+
+        rescue Exception => e
+          LOGGER.debug "get_props_cdk failed with '" + $! + "'"
+        end
+
+        #[ matrix, row ]
+
+      end
+
+
       # Get X and Y size of a nested Array (Matrix)
       # @param [Array] Two-dimensional ruby array (matrix) with X and Y size > 0
       # @return [Arrray] X and Y size of the matrix
@@ -145,8 +230,8 @@ module OpenTox
         #puts "NRC: #{nr_cases}, NRF: #{nr_features}"
         [ nr_cases, nr_features ]
       end
-      
-      
+
+
       # Get confidence for regression, with standard deviation of neighbor activity if conf_stdev is set.
       # @param[Hash] Required keys: :sims, :acts, :neighbors, :conf_stdev
       # @return[Float] Confidence
