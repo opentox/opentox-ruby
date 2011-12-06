6bb055dd22a75492e82c0765d8d945f99b9c7b05
Andreas Maunz <andreas@maunz.de>
2011-12-06 13:43:02 +0100

    Added outlier detection for mlr

--- lib/algorithm.rb
+++ lib/algorithm.rb
@@ -375,6 +375,8 @@ module OpenTox
           maxcols = params[:maxcols]
 
           nr_cases, nr_features = get_sizes n_prop
+          maxcols = nr_features if maxcols > nr_features
+
           data_matrix = GSL::Matrix.alloc(n_prop.flatten, nr_cases, nr_features)
           query_matrix = GSL::Matrix.alloc(q_prop.flatten, 1, nr_features) # same nr_features
 
@@ -404,6 +406,42 @@ module OpenTox
           
           ### Model
           @r = RinRuby.new(false,false)   # global R instance leads to Socket errors after a large number of requests
+          @r.eval "suppressPackageStartupMessages(library(\"robustbase\"))"
+          @r.eval "outlier_threshold = 0.975"
+
+          # outlier removal -- changes cases; adjust acts accordingly (stop if query is outlier)
+          outliers = []
+          begin
+            LOGGER.debug "Outliers..."
+            @r.q = query_matrix.to_a.flatten
+            @r.odx = data_matrix.to_a.flatten
+            @r.eval "odx <- matrix(odx, #{nr_cases}, #{nr_features}, byrow=T)"
+            @r.eval "odx <- rbind(q,odx)" # query is nr 0 (1) in ruby (R)
+            @r.eval 'mah <- covMcd(odx)$mah' # run mcd alg
+            @r.eval "mah <- pchisq(mah,#{nr_features})"
+            LOGGER.debug("p-values: " + @r.mah.collect{|v| sprintf("%.2f", v)}.join(", "))
+            @r.eval 'outliers <- which(mah>outlier_threshold)'
+            outliers = @r.outliers.to_a.collect{|v| v-2 } # translate to ruby index (-1 for q, -1 due to ruby)
+          rescue Exception => e
+            LOGGER.debug "#{e.class}: #{e.message}"
+            LOGGER.debug "Backtrace:\n\t#{e.backtrace.join("\n\t")}"
+          end
+          LOGGER.debug "Detected #{outliers.size} outliers: [#{outliers.join(", ")}]"
+          if (outliers.include?(-1))
+            raise "Query is an outlier."
+          end
+          temp_dm = []; temp_acts = []
+          data_matrix.to_a.each_with_index { |elem, idx| temp_dm << elem unless outliers.include? idx }
+          nr_cases, nr_features = get_sizes temp_dm
+          data_matrix = GSL::Matrix.alloc(temp_dm.flatten, nr_cases, nr_features)
+          acts.each_with_index { |elem, idx| temp_acts << elem unless outliers.include? idx }
+          acts = temp_acts # same nr_features
+
+          # optimize selection of training instances -- TO COME
+          
+
+
+          # build model on best selection
           @r.x = data_matrix.to_a.flatten
           @r.q = query_matrix.to_a.flatten
           @r.y = acts.to_a.flatten
@@ -548,6 +586,7 @@ module OpenTox
               best[[5]] = r2Loo[ncompLoo]
             }"
           end
+
 
           # build model on best selection
           @r.eval "best_values = c(best[[1]], best[[2]], best[[3]], best[[5]])" # Ruby-index: 0-nr neighbors, 1-nr components, 2-RMSE, 3-R2
