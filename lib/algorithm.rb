8b4bf751d013e7890842e4672fd2e6606e05b1b5
Andreas Maunz <andreas@maunz.de>
2011-12-12 10:41:30 +0100

    y outlier removal also for pcr

--- lib/algorithm.rb
+++ lib/algorithm.rb
@@ -587,7 +587,7 @@ module OpenTox
           @r = RinRuby.new(false,false)   # global R instance leads to Socket errors after a large number of requests
           @r.eval 'suppressPackageStartupMessages(library("pls"))'
           @r.eval 'suppressPackageStartupMessages(library("robustbase"))'
-          @r.eval 'outlier_threshold = 0.975'
+          @r.eval 'outlier_threshold = 0.999'
 
 
           # outlier removal -- changes cases; adjust acts accordingly (stop if query is outlier)
@@ -596,6 +596,7 @@ module OpenTox
             LOGGER.debug "Outliers..."
             @r.q = query_matrix.to_a.flatten
             @r.odx = data_matrix.to_a.flatten
+            @r.y = acts.to_a.flatten
             @r.eval "odx <- matrix(odx, #{nr_cases}, #{nr_features}, byrow=T)"
             @r.eval 'odx <- rbind(q,odx)' # query is nr 0 (1) in ruby (R)
             @r.eval 'mah <- covMcd(odx)$mah' # run mcd alg
@@ -603,6 +604,14 @@ module OpenTox
             LOGGER.debug("p-values: " + @r.mah.collect{|v| sprintf("%.2f", v)}.join(", "))
             @r.eval 'outliers <- which(mah>outlier_threshold)'
             outliers = @r.outliers.to_a.collect{|v| v-2 } # translate to ruby index (-1 for q, -1 due to ruby)
+
+            @r.eval 'fqu = matrix(summary(y))[2]'
+            @r.eval 'tqu = matrix(summary(y))[5]'
+            @r.eval 'outliers <- which(y>(tqu+1.5*IQR(y)))' # univariate outliers due to Tukey (http://goo.gl/mwzNH)
+            outliers << @r.outliers.to_a.collect{|v| v-1 } # translate to ruby index (-1 due to ruby)
+            @r.eval 'outliers <- which(y<(fqu-1.5*IQR(y)))'
+            outliers << @r.outliers.to_a.collect{|v| v-1 }
+            outliers.flatten!
           rescue Exception => e
             LOGGER.debug "#{e.class}: #{e.message}"
             LOGGER.debug "Backtrace:\n\t#{e.backtrace.join("\n\t")}"
